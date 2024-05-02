5e0cdfca8e7de79e528ce50cd29713301a9a0704
Vladimir Starcevic <vstarcevic@gmail.com>
2024-05-02 14:36:46 +0200

    add frontend html and style

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+ function copyLink() {
+    var copyText = document.getElementById("shortUrl");
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  }
